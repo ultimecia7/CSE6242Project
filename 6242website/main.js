255b17f72e723ee58f12576db162dd6ad09b665a
ultimecia7 <ultimecia7@gmail.com>
2017-12-08 16:14:58 -0500

    New version of front end UI

--- 6242website/main.js
+++ 6242website/main.js
@@ -1,6 +1,6 @@
 function SongInfo(name, artist) {
-    this.name = name;
-    this.artist = artist;
+    this.song = name;
+    this.singer = artist;
 }
 
 function search_song(name,id){
@@ -29,6 +29,7 @@ function retrievePath(source,target){
 	$.get("index.php?"+str, function(data){
 		var array = data.split(data_delimeter);
 		var songinfo = [];
+		d3.select("#mainButton").attr("value", "Loading")
 		for(i=0; i<array.length; i++){
 			parts = array[i].split(attribute_delimeter);
 			songinfo.push(new SongInfo(parts[0], parts[1]));
@@ -43,188 +44,216 @@ var margin = {top: 30, right: 20, bottom: 30, left: 30},
 
 var y_position = 200
 var mainCircleRadius = 150
-var mainCircles = [
- 	{ "color" : "#EBEEAF" , "song" : "Bad Blood", "singer": "Taylor Swift"},
- 	{ "color" : "#FE7BBA", "song" : "Symphony No. 9", "singer": "Beethoven"}];
 
 var pathNodes = [
- 	{"color" : "#A9F1A5", "song" : "Rise", "singer": "Katy Perry" },
- 	{"color" : "#9BF4E5", "song" : "It's A Hard Life", "singer": "Queen"},
- 	{ "color" : "#90ACF7","song" : "Road To Joy", "singer": "Bright Eyes" },
- 	{"color" : "#CE86FA", "song" : "Ode to Joy 1", "singer": "Beethoven"}];
+ 	{"song" : "Rise", "singer": "Katy Perry" },
+ 	{"song" : "It's A Hard Life", "singer": "Queen"},
+ 	{"song" : "Road To Joy", "singer": "Bright Eyes" },
+ 	{"song" : "Road To Joy", "singer": "Bright Eyes" },
+ 	{"song" : "Road To Joy", "singer": "Bright Eyes" },
+ 	{"song" : "Road To Joy", "singer": "Bright Eyes" }];
 
-var similarNodes = [
+var similarNodes = [[
  	{"song" : "Skyscraper", "singer": "Demi Lovato" },
  	{"song" : "The Climb", "singer": "Miley Cyrus"},
  	{"song" : "Gold Steps", "singer": "Neck Deep" },
  	{"song" : "Defying Gravity", "singer": "Idina Menzel"},
  	{"song" : "Save Myself", "singer": "Ed Sheeran"}
- 	];
+ 	],
+ 	[
+ 	{"song" : "Skyscraper", "singer": "Demi Lovato" },
+ 	{"song" : "The Climb", "singer": "Miley Cyrus"},
+ 	],
+ 	[
+ 	{"song" : "Defying Gravity", "singer": "Idina Menzel"},
+ 	{"song" : "Save Myself", "singer": "Ed Sheeran"}
+ 	],
+ 	[],
+ 	[
+ 	{"song" : "Skyscraper", "singer": "Demi Lovato" },
+ 	{"song" : "The Climb", "singer": "Miley Cyrus"},
+ 	{"song" : "Gold Steps", "singer": "Neck Deep" },
+ 	],
+ 	[]];
 
-// var input = d3.select("body").select("#input").append('input')
-//     .attr('type','text')
-//     .attr('name','textInput')
-//     .attr('value','Text goes here')
-//     .style("padding-top: 50px");
+pathNodeStart = 100
+pathNodeEnd = 1000
+pathTranstionTime = 750
 
-// var input2 = d3.select("body").select("#input").append('input')
-//     .attr('type','text')
-//     .attr('name','textInput')
-//     .attr('value','Text goes here')
-//     .style("padding-top: 100px");
+pathnodeSpace = (pathNodeEnd-pathNodeStart)/(pathNodes.length+1)
+pathNodeRadius = 0.7*pathnodeSpace/2
+similarNodeRadius = 0.6*pathNodeRadius
 
-var svg = d3.select("body").select("#viz")
-    .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
+// ["#EBEEAF", "#FE7BBA"]
+function startPage() {
+	var mainCircleColors = ["#EBEEAF", "#FE7BBA"]
+	f=d3.interpolateHsl(mainCircleColors[0], mainCircleColors[1])
+	var svg = d3.select("body").select("#viz")
+	    .append("svg")
+	        .attr("width", width + margin.left + margin.right)
+	        .attr("height", height + margin.top + margin.bottom);
 
-createNode(svg, 300, y_position, mainCircleRadius, mainCircles[0].color, "mainCircleContainer1", "mainCircle1");
-d3.select("#mainCircleContainer1")
-addNodeLabel("#mainCircleContainer1", "Got something you really like?", "");
-createNode(svg, 800, y_position, mainCircleRadius, mainCircles[1].color, "mainCircleContainer2", "mainCircle2");
-addNodeLabel("#mainCircleContainer2", "We'll find something you don't like!", "");
-
-
-for (i = 0; i < pathNodes.length; i++) {
-	createNode(svg, 100 + (i+1)*180, y_position, 0, pathNodes[i].color, "pathCircleContainer" + i, "pathCircle");
-
-	d3.select("#pathCircleContainer" + i).selectAll(".pathCircle")
-        .on('mouseover', function () {
-        	addSimilar("#pathCircleContainer1", "similarContainer");
-        	console.log("#pathCircleContainer" + i)
-        })
-        .on('mouseout', function() {removeSimilar()});
-	// var pathContainer = svg.append("g")
-	// 						.attr("transform", "translate(" + (margin.left + 100 + (i+1)*180) + "," + (margin.top + 170) + ")")
-	// 						.attr("class", "mainCircleContainer" + i)
-	// pathContainer.data([pathNodes[i]]).append("circle")
+	createNode(svg, margin.left + 300, margin.top + y_position, mainCircleRadius, mainCircleColors[0], "mainCircleContainer1", "mainCircle1");
+	d3.select("#mainCircleContainer1")
+	addNodeLabel("#mainCircleContainer1", "Got something you really like?", "");
+	createNode(svg, margin.left + 800, margin.top + y_position, mainCircleRadius, mainCircleColors[1], "mainCircleContainer2", "mainCircle2");
+	addNodeLabel("#mainCircleContainer2", "We'll find something you don't like!", "");
 }
 
-function createNode(parent, x, y, radius, color, containerId, circleClass, containerClass = "container") {
-	var mainCircle1 = parent.append("g").attr("transform", "translate(" + (margin.left + x) + "," + (margin.top + y) + ")")
+function createNode(parent, x, y, radius, color, containerId, circleClass, containerClass = "container", make_hidden_circle = false) {
+	var mainCircle1 = parent.append("g").attr("transform", "translate(" + x + "," + y + ")")
 						.attr("id", containerId).attr("class", containerClass)
-
+	if (make_hidden_circle) {
+	mainCircle1.append("circle")
+              .attr("r", pathNodeRadius+2*similarNodeRadius)
+              .attr("class", "pathCircle_hidden")
+              .attr('fill-opacity', 0);
+    }
 	mainCircle1.append("circle")
                   .attr("r", radius)
                   .attr("class", circleClass)
                   .style("fill", color);
-	// mainCircle1.append("text")
-	// 		.attr("dy", function(d){return -10})
-	// 		.attr("text-anchor", "middle")
-	// 		.text(function(d) {return song})
-
-	// mainCircle1.append("text")
-	// 		.attr("dy", function(d){return +10})
-	// 		.attr("text-anchor", "middle")
-	// 		.text(function(d) {return singer})
 }
 
 function addNodeLabel(containerId, song, singer, fontsize = 15) {
-	console.log(containerId);
 	d3.select(containerId).selectAll("text").remove();
 	var container = d3.select(containerId);
 	container.append("text")
 			.attr("font-size",fontsize + "px")
 			.attr("dy", function(d){return -5})
 			.attr("text-anchor", "middle")
-			.text(function(d) {return song});
+			.text(function(d) {return song})
+			.style("pointer-events", "none");
 
 	container.append("text")
 			.attr("font-size",fontsize + "px")
 			.attr("dy", function(d){return +15})
 			.attr("text-anchor", "middle")
-			.text(function(d) {return singer});
+			.text(function(d) {return singer})
+			.style("pointer-events", "none");
 }
 
 function updateData(path) {
-	sourceSong = path[0];
-	targetSong = path[path.length-1];
-    // Select the section we want to apply our changes to
-    d3.select("#mainButton") 
-            // .duration(750)
-        .attr("value", "Back");
-    addNodeLabel("#mainCircleContainer1", sourceSong.name, sourceSong.artist);
-    addNodeLabel("#mainCircleContainer2", targetSong.name, targetSong.artist);
+	mainCircles = [path[0], path[path.length-1]];
+	nNodePlacement = 6;
+
+	var nPathNodes = path.length-2;
+	if(nPathNodes > nNodePlacement){
+		pathNodes = []
+		similarNodes = []
+		nSmallCircles = (nPathNodes-nNodePlacement)/nNodePlacement;
+		var index = 1;
+		for(i=0;i<nNodePlacement;i++){
+			if(index >= path.length) break;
+			pathNodes.push(path[index++]);
+			curSimNodes = []
+			for(j=0;j<nSmallCircles;j++){
+				if(index >= path.length) break;
+				curSimNodes.push(path[index++]);
+			}
+			similarNodes.push(curSimNodes);
+		}
+		console.log(pathNodes);
+		console.log(similarNodes);
+	} else {
+		pathNodes = path.slice(1,path.length-1);
+		similarNodes = []
+	}
+	pathNodesColors = []
+	var nColors=pathNodes.length;
+	for (var i=0; i<nColors; i++) {pathNodesColors.push(f(i/(nColors-1)))};
+	similarNodesColor = f(Math.random()*nColors/nColors)
+    for (i = 0; i < pathNodes.length; i++) {
+		createNode(d3.select("body").select("#viz").select('svg'), margin.left + pathNodeStart + (i+1)*pathnodeSpace, margin.top + y_position, 0, pathNodesColors[i], "pathCircleContainer" + i, "pathCircle", "container", true)
+		d3.select("#pathCircleContainer" + i).selectAll(".pathCircle")
+	        .on('mouseover', function () {
+	        	removeSimilar()
+	        	var nodeId = parseInt(this.parentNode.id.substr(this.parentNode.id.length - 1));
+	        	addSimilar("#" + this.parentNode.id, "similarContainer", nodeId, similarNodes);
+	        	d3.select("body").select("#viz").select('svg').selectAll(".similarNode").style("pointer-events", "none");
+	        })
+	    d3.select("#pathCircleContainer" + i).selectAll(".pathCircle_hidden").on('mouseout', function() {
+	    	removeSimilar()})
+	}
+
+    d3.select("#mainButton")
+        .attr("value", "Back")
+        .attr("onclick", "backToHome()");
+    addNodeLabel("#mainCircleContainer1", mainCircles[0].song, mainCircles[0].singer)
+    addNodeLabel("#mainCircleContainer2", mainCircles[1].song, mainCircles[1].singer)
     var svg = d3.select("body").transition();
 
-    // // Make the changes
-        svg.select(".mainCircle1")   // change the line
-            .duration(750)
-            .attr("r", 60);
+    svg.select(".mainCircle1")
+        .duration(pathTranstionTime)
+        .attr("r", pathNodeRadius);
 
-                svg.select("#mainCircleContainer1")   // change the line
-            .duration(750)
-			.attr("transform", "translate(" + (margin.left + 100) + "," + (margin.top + y_position) + ")");
+            svg.select("#mainCircleContainer1")
+        .duration(pathTranstionTime)
+		.attr("transform", "translate(" + (margin.left + pathNodeStart) + "," + (margin.top + y_position) + ")");
 
-		svg.select(".mainCircle2")   // change the line
-            .duration(750)
-            .attr("r", 60);
+	svg.select(".mainCircle2")
+        .duration(pathTranstionTime)
+        .attr("r", pathNodeRadius);
 
-                svg.select("#mainCircleContainer2")   // change the line
-            .duration(750)
-			.attr("transform", "translate(" + (margin.left + 1000) + "," + (margin.top + y_position) + ")");
+            svg.select("#mainCircleContainer2")
+        .duration(pathTranstionTime)
+		.attr("transform", "translate(" + (margin.left + pathNodeEnd) + "," + (margin.top + y_position) + ")");
 
-		svg.selectAll(".pathCircle")   // change the line
-	            .duration(750)
-	            .attr("r", 60);
-
-		var count = 0;
-		for (i = 1; i < path.length-1; i++) {
-	        addNodeLabel("#pathCircleContainer" + (i-1), path[i].name, path[i].artist);
-			if(++count == pathNodes.length) break;
-		}
-
-
-	// createNode(d3.select("#pathCircleContainer1"), 20, 20, 10, 'red', "similarContainer1", "similarNode")
-
-        // svg.select(".mainCircle2")   // change the line
-        //     .duration(750)
-        //     .attr("cx", 1000)
-        //     .attr("cy", 170)
-        //     .attr("r", 60);
-
-
-
-        // d3.selectAll(".pathCircle")
-        // .on('mouseover', function () { addSimilar(d3.select(this.parentNode), "similarContainer") })
-        // .on('mouseout', function() {removeSimilar()})
-
+	svg.selectAll(".pathCircle")
+            .duration(pathTranstionTime)
+            .attr("r", pathNodeRadius);
+	for (i = 0; i < pathNodes.length; i++) {
+        addNodeLabel("#pathCircleContainer" + i, pathNodes[i].song, pathNodes[i].singer)
+        }
 }
 
 
-function addSimilar(parentId, containerId) {
-	// var pathCircles = similarGroup.append("circle")
- //          .attr("cx", node.x_axis + 20)
- //          .attr("cy", node.y_axis + 50)
- //          .attr("r", 0)
- //          .attr("class", "similarNode")
- //          .style("fill", 'red');
-	// createNode(d3.select(parentId), 20, 20, 10, 'red', containerId, "similarNode")
-	createNode(d3.select(parentId), 40, 40, 0, '#33cccc', "similarContainer1", "similarNode", containerClass = "similarContainer")
-	addNodeLabel("#similarContainer1", similarNodes[0].song, similarNodes[0].singer, fontsize = 10);
-	createNode(d3.select(parentId), -70, 65, 0, '#33cccc', "similarContainer2", "similarNode", containerClass = "similarContainer")
-	addNodeLabel("#similarContainer2", similarNodes[1].song, similarNodes[1].singer, fontsize = 10);
-	createNode(d3.select(parentId), 60, -70, 0, '#33cccc', "similarContainer3", "similarNode", containerClass = "similarContainer")
-	addNodeLabel("#similarContainer3", similarNodes[2].song, similarNodes[2].singer, fontsize = 10);
-	createNode(d3.select(parentId), -30, -130, 0, '#33cccc', "similarContainer4", "similarNode", containerClass = "similarContainer")
-	addNodeLabel("#similarContainer4", similarNodes[3].song, similarNodes[3].singer, fontsize = 10);
-	createNode(d3.select(parentId), -120, -80, 0, '#33cccc', "similarContainer5", "similarNode", containerClass = "similarContainer")
-	addNodeLabel("#similarContainer5", similarNodes[4].song, similarNodes[4].singer, fontsize = 10);
+function addSimilar(parentId, containerId, nodeId, similarNodes) {
+
+	var totalRadius = pathNodeRadius + similarNodeRadius
+ 	var similar_songs = similarNodes[nodeId]
+ 	var bucket = [];
+
+	for (var i=0; i<similar_songs.length; i++) {
+	    bucket.push(i);
+	}
+
+	function getRandomFromBucket() {
+	   var randomIndex = Math.floor(Math.random()*bucket.length);
+	   return bucket.splice(randomIndex, 1)[0];
+	}
+
+ 	for (i = 0; i < similar_songs.length; i++) {
+ 		var offset = Math.random()*2-1
+ 		var degree = (360/similar_songs.length*getRandomFromBucket() + offset*360/similar_songs.length/(2*similar_songs.length))*Math.PI/180
+ 		createNode(d3.select(parentId),totalRadius*Math.cos(degree), totalRadius*Math.sin(degree), 0, similarNodesColor, "similarContainer" + i, "similarNode", containerClass = "similarContainer")
+		addNodeLabel("#similarContainer" + i, similar_songs[i].song, similar_songs[i].singer, fontsize = 10);
+ 	}
 
     var svg = d3.select("body").transition();
     	svg.selectAll(".similarNode")
-    		.duration(750)
-    		.attr("r", 35)
-
+    		.duration(250)
+    		.attr("r", similarNodeRadius)
 }
 
 
 function removeSimilar() {
-    // var svg = d3.select("body").transition();
-
-    // svg.selectAll(".similarNode")
-    // 	.attr("r", 0)
-
    	d3.selectAll(".similarContainer").remove()
-
 }
+
+function backToHome() {
+	d3.select("body").select("#viz").selectAll("*").remove();
+	d3.select("#mainButton")
+        .attr("value", "Go")
+        .attr("onclick", "updateData()");
+	startPage()
+}
+
+function getColor(){
+  r = Math.random()*155 + 100
+  g = Math.random()*155 + 100
+  b = Math.random()*155 + 100
+  return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
+
+startPage()
